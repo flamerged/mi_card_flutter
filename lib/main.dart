33aca53a313f13ee3bfb8e1be6e4a310983b023f
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-09 06:23:41 +0100

    completed layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
