ff8fb34e80c79b4a5466e7bb6e29aff6fa4bd278
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-10 19:13:41 +0100

    added name and profession, corrected app names

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,22 +9,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: double.infinity,
-            ),
-            CircleAvatar(
-              radius: 50.0,
-              foregroundImage: AssetImage('images/profile.jpg'),
-            )
-          ],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: double.infinity,
+              ),
+              CircleAvatar(
+                radius: 50.0,
+                foregroundImage: AssetImage('images/profile.jpg'),
+              ),
+              Text(
+                "Mersad Ajanovic",
+                style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                "FULLSTACK DEVELOPER",
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontFamily: "Source Sans Pro"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
