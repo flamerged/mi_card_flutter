15b432bd19770fab0ac1bfeb42066eda615cd8a8
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-10 19:43:44 +0100

    added fake phone and mail info

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,7 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
-                width: double.infinity,
-              ),
               CircleAvatar(
                 radius: 50.0,
                 foregroundImage: AssetImage('images/profile.jpg'),
@@ -35,9 +31,57 @@ class MyApp extends StatelessWidget {
               Text(
                 "FULLSTACK DEVELOPER",
                 style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+                    fontSize: 20,
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
                     fontFamily: "Source Sans Pro"),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+49 175 238023020",
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "Source Sans Pro",
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "mersad@mersad.com",
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "Source Sans Pro",
+                          fontSize: 20),
+                    )
+                  ],
+                ),
               )
             ],
           ),
